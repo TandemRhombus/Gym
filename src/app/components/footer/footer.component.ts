7d0529877a16ace3a9154a59ce1749499aa707eb
Sergio Emiliano Hernández Villalpando <96132883+TandemRhombus@users.noreply.github.com>
2025-04-30 16:30:00 -0600

    actualizacion de logos

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [ CommonModule, FontAwesomeModule ],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {}
+export class FooterComponent {
+  faFacebook = faFacebookF;
+  faTwitter = faTwitter;
+  faInstagram = faInstagram;
+}
